1353d8356962df4539dd29f55e25c92e3f762345
Ilyxsss <ilyas.semgane@univ-tlse3.fr>
2026-04-16 13:23:46 +0200

    Création de la classe Humain

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -1,5 +1,14 @@
 package histoire;
 
-public class HistoireTP4 {
+import personnages.Humain;
 
+public class HistoireTP4 {
+	public static void main(String[] args) {
+		Humain professeur = new Humain("Prof", "kombucha", 54);
+		professeur.direBonjour();
+		professeur.acheter("une boisson", 12);
+		professeur.boire();
+		professeur.acheter("un jeu", 2);
+		professeur.acheter("un kimono", 50);
+	}
 }
