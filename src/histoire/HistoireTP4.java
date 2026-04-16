3b2ed085f346d65aacedf9618f892620d894cd41
Ilyxsss <ilyas.semgane@univ-tlse3.fr>
2026-04-16 14:03:49 +0200

    Création de la classe Ronin

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -2,6 +2,7 @@ package histoire;
 
 import personnages.Commercant;
 import personnages.Humain;
+import personnages.Ronin;
 import personnages.Yakuza;
 
 public class HistoireTP4 {
@@ -22,5 +23,9 @@ public class HistoireTP4 {
 		Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
 		yaku.direBonjour();
 		yaku.extorquer(marco);
+		
+		Ronin ronin = new Ronin("Roro", "shochu", 60);
+		ronin.direBonjour();
+		ronin.donner(marco);
 	}
 }
---
+++ src/personnages/Ronin.java
@@ -0,0 +1,21 @@
+package personnages;
+
+public class Ronin extends Humain {
+	private int honneur = 1;
+
+	public Ronin(String nom, String boissonFavorite, int argent) {
+		super(nom, boissonFavorite, argent);
+	}
+	
+	public int getHonneur() {
+		return honneur;
+	}
+	
+	public void donner(Commercant beneficiaire) {
+		int don = getArgent() * 10 / 100;
+		parler(beneficiaire.getNom() + " prend ces " + don + " sous.");
+		beneficiaire.recevoir(don);
+		perdreArgent(don);
+		honneur++;
+	}
+}
